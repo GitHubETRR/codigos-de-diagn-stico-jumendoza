801474efd364fbde1af0eb32d93a0fae80b8c4b2
jumendoza <juanagustinm077@gmail.com>
2025-09-04 09:08:14 -0300

    Create primeraPrueba.cpp

---
+++ primeraPrueba.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numero;
+    int final;
+    cout << "Ingrese el valor de su numero\n";
+    cin >> numero;
+    final = numero *2;
+    cout << "El doble de su numero es " << final << "\n";
+    return 0;
+}
